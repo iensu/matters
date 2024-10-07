66a42c0d855e0a1a3c611b491887571f5989164e
iensu <jostlund@gmail.com>
2024-10-07 21:40:12 +0000

    Deploying to gh-pages from @ iensu/matters@946cb3b81692f1ab6814e4d3426859b43e3bfd19 🚀

--- main.js
+++ main.js
@@ -11,10 +11,10 @@ const OPERATION_STRINGS = {
   [FLAG_DIV]: "/",
 };
 
-let NUM_PROBLEMS = 20;
+let NUM_PROBLEMS = 30;
 let MAX_RESULT = 20;
 let MAX_FACTOR = 10;
-let OPERATIONS = FLAG_ADD + FLAG_SUB;
+let OPERATIONS = FLAG_ADD + FLAG_SUB + FLAG_MUL;
 
 const createInstance = async (path, importObject = {}) => {
   const { instance } = await WebAssembly.instantiateStreaming(
@@ -47,12 +47,72 @@ const generateMathProblems = () => {
   while (index < view.length) {
     const [op, x, y] = view.slice(index, index + PROBLEM_OBJECT_SIZE);
 
-    results.push({ op: OPERATION_STRINGS[op], x, y });
+    results.push({ op, x, y });
 
     index += PROBLEM_OBJECT_SIZE;
   }
 
   return results;
+};
+
+const render = (renderFn, ...args) => {
+  const template = document.createElement("template");
+
+  template.innerHTML = renderFn(...args);
+
+  if (template.content.childNodes.length > 1) {
+    return [...template.content.childNodes];
+  }
+
+  return template.content.firstChild;
+};
+
+const renderProblem = (label, problem) => {
+  return `
+    <div class="problem">
+      <label>${label}</label>
+      <span class="number">${problem.x}</span>
+      <span class="operation">${OPERATION_STRINGS[problem.op]}</span>
+      <span class="number">${problem.y}</span>
+      <span class="equals">=</span>
+      <input type=""text" class="answer-input" inputmode="numeric" />
+    </div>
+`;
+};
+
+const renderProblems = (problems) =>
+  problems.map((problem, index) => renderProblem(index + 1, problem)).join("");
+
+const onSubmit = (problems) => (event) => {
+  event.preventDefault();
+
+  const answers = document.querySelectorAll(".answer-input");
+
+  const evaluate = (problem) => {
+    switch (problem.op) {
+      case FLAG_ADD:
+        return problem.x + problem.y;
+      case FLAG_SUB:
+        return problem.x - problem.y;
+      case FLAG_MUL:
+        return problem.x * problem.y;
+      case FLAG_DIV:
+        return problem.x / problem.y;
+    }
+  };
+
+  answers.forEach((answerElem, index) => {
+    answerElem.classList.remove("correct");
+    answerElem.classList.remove("incorrect");
+    const answer = Number(answerElem.value);
+    const problem = problems[index];
+
+    if (!problem) {
+      console.error("No problem for answer!", index);
+    }
+    const result = evaluate(problem) === answer ? "correct" : "incorrect";
+    answerElem.classList.add(result);
+  });
 };
 
 (async () => {
@@ -74,12 +134,22 @@ const generateMathProblems = () => {
 
   const problems = generateMathProblems();
 
-  const list = document.getElementById("problems");
+  const form = document.getElementById("problems");
 
-  for (const problem of problems) {
-    const problemString = `${problem.x} ${problem.op} ${problem.y} = _______`;
-    const element = document.createElement("li");
-    element.innerText = problemString;
-    list.appendChild(element);
+  form.addEventListener("submit", onSubmit(problems));
+
+  for (const node of render(renderProblems, problems)) {
+    node.addEventListener("keydown", (event) => {
+      event.target.classList.remove("correct");
+      event.target.classList.remove("incorrect");
+    });
+    form.appendChild(node);
   }
+
+  const correctButton = render(
+    () => `<button id="correct-button" type="submit">Rätta</button>`,
+  );
+  correctButton.addEventListener("click", onSubmit(problems));
+
+  form.appendChild(correctButton);
 })();
